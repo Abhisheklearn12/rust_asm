eca1691854efd55269c32735378320ae4774fe18
Abhisheklearn12 <cseabhishek123@gmail.com>
2025-12-27 13:50:47 +0530

    feat: added comment

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     println!("  add(42, 13) = {}", add(42, 13));
     println!("  multiply(7, 6) = {}", multiply(7, 6));
 
+    // Added "Factorial Function"
     println!("\nRecursive factorial:");
     println!("  5! = {}", factorial(5));
     println!("  10! = {}", factorial(10));
